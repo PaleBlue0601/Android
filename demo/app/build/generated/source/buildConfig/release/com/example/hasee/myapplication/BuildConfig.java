f6925256347017e6d9e312e2fb6fe2660ee17756
WHBhorse <1876252223@qq.com>
2019-10-21 17:38:17 +0800

    第三次提交

---
+++ demo/app/build/generated/source/buildConfig/release/com/example/hasee/myapplication/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.hasee.myapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.hasee.myapplication";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
